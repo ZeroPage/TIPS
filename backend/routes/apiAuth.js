049e083e9aec7a9d2aed93ae5b37daf1186d643f
DESKTOP-A1G3F98\InseoPark <wb092311@naver.com>
2020-11-18 11:07:39 +0900

    auth 관련 수정

--- backend/routes/apiAuth.js
+++ backend/routes/apiAuth.js
@@ -3,13 +3,14 @@ var router = express.Router();
 var passport = require('passport');
 
 router.post('/login', passport.authenticate('local'), function(req, res, next) {
-  res.json({ success: 'ok' });
+  res.end();
 });
 
 router.post('/logout', function(req, res, next) {
   req.logout();
-  req.session.save(() => {
-    res.json({ success: 'ok' });
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid');
+    res.end();
   });
 });
 
