85887a2897dd2acb3259bf53c2e4631128d00feb
DESKTOP-A1G3F98\InseoPark <wb092311@naver.com>
2020-12-02 05:07:58 +0900

    게시판 시작

--- frontend/src/views/Board/BoardList.js
+++ frontend/src/views/Board/BoardList.js
@@ -16,186 +16,359 @@
 
 */
 import React from "react";
+import queryString from 'query-string';
 
 // reactstrap components
-import { Button, ButtonGroup, Badge, Card, Container, Form, Row, Col, ListGroup, ListGroupItem, Pagination, PaginationItem, PaginationLink } from "reactstrap";
+import { Button, Badge, Card, Container, Form, Row, Col,
+  FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText,
+  ListGroup, ListGroupItem, Pagination, PaginationItem, PaginationLink, Progress } from "reactstrap";
 
 // core components
 import SimpleNavbar from "components/Navbars/SimpleNavbar.js";
+import Template from "views/Template.js";
 
 class BoardList extends React.Component {
   constructor(props) {
     super(props);
+
+    const { search } = this.props.location;
+    const queryObj = queryString.parse(search);
+    const { category_id, page, order, search_text } = queryObj;
+
     this.state = {
+      board_id: this.props.match.params.id,
+      category_id: category_id,
+      page: page,
+      order: order,
+      search: search_text,
+      search_text: search_text,
       items: [],
-      category: [],
-      isLogin: true
+      categories: [],
+      count: 0,
+      is_admin: false,
+      is_prime: false
     };
+
+    this.getLogin();
+    this.getCategory();
+    this.getDocument();
+
+    this.handleCategoryId = this.handleCategoryId.bind(this);
+    this.handleSearchText = this.handleSearchText.bind(this);
   }
 
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+
+  handleCategoryId(event) {
+    this.setState({category_id: event.target.value});
+    this.redirect(event.target.value, this.state.page, this.state.order, this.state.search);
+  }
+
+  handleSearchText(event) {
+    console.log(event.target.value);
+    this.setState({search_text: event.target.value});
+  }
+
+  redirectUrl(category_id, page, order, search) {
+    var redirect_url = "?";
+    if(category_id) redirect_url = redirect_url + `category_id=${category_id}&`;
+    if(page) redirect_url = redirect_url + `page=${page}&`;
+    if(order) redirect_url = redirect_url + `order=${order}&`;
+    if(search) redirect_url = redirect_url + `search_text=${search}&`;
+    return redirect_url;
+  }
+
+  redirect(category_id, page, order, search) {
+    this.props.history.push(this.redirectUrl(category_id, page, order, search));
+    window.location.reload();
+  }
+
+  getLogin() {
+    fetch("/api/v1/members", {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(
+      (result) => {
+        if(result.ok) {
+          result.json().then(data => {
+            this.setState({
+              is_admin: data.is_admin,
+              is_prime: data.is_prime
+            });
+          });
+        }
+        else {
+          alert("로그인 후 이용해주세요.");
+          this.props.history.push('/');
+        }
+      }
+    );
+  }
+
+  getCategory() {
+    var fetch_url = "​/api​/v1​/boards​/" + this.state.board_id + "/categories";
+    console.log(fetch_url);
+    fetch(fetch_url, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(
+      (result) => {
+        if(result.ok) {
+          result.json().then(data => {
+            console.log(data.results);
+            this.setState({categories: data.results});
+          });
+        }
+      }
+    );
+  }
+
+  getDocument() {
+    var fetch_url = `/api/v1/documents?board_id=${this.state.board_id}`;
+    if(parseInt(this.state.category_id)) fetch_url = fetch_url + `category_id=${this.state.category_id}&`;
+    if(parseInt(this.state.page)) fetch_url = fetch_url + `page=${this.state.page}&`;
+    if(this.state.order) fetch_url = fetch_url + `order=${this.state.order}&`;
+    if(this.state.search) fetch_url = fetch_url + `title=${this.state.search}&`;
+
+    fetch(fetch_url, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(
+      (result) => {
+        if(result.ok) {
+          result.json().then(data => {
+            this.setState({
+              items: data.results,
+              count: parseInt(data.total_count)
+            });
+          });
+        }
+      }
+    );
+  }
+
+  getCategoryName(find_id) {
+    if(!find_id) return "전체";
+
+    for(var i=0;i<this.state.categories.length;i++) {
+      if(this.state.categories[i].category_id === find_id) {
+        return this.state.categories[i].name;
+      }
+    }
+    return "";
+  }
+
+  getList() {
+    var ret = [];
+    this.state.items.forEach(item => {
+      ret.push(
+        <ListGroupItem>
+          <Row>
+            <Col xs="12">
+              <Row>
+                <Col xs="3">No. {item.document_id}</Col>
+                <Col xs="9">
+                  <a href={`/board-view-page/${item.document_id}`}>
+                    {item.title}
+                  </a>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs="8">
+                  <Badge className="text-uppercase" color="primary" pill>
+                    {this.getCategoryName(parseInt(item.category_id))}
+                  </Badge>
+                </Col>
+                <Col xs="4">
+                  <div className="text-right">
+                    게시일 : {item.created.substring(0, 10)}
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </ListGroupItem>
+      );
+    });
+    return ret;
+  }
+
+  getPagination() {
+    const TOTAL_PAGE = parseInt(this.state.count / 10) + 1;
+    const PAGE_NUM = 2;
+    var page_id = parseInt(this.state.page);
+    if(!page_id) page_id = 1; //id값이 없으면 기본값 1
+
+    var ret = [];
+    ret.push(
+      <PaginationItem>
+        <PaginationLink first href={this.redirectUrl(this.state.category_id, 1, this.state.order, this.state.search)} />
+      </PaginationItem>
+    );
+    if(page_id != 1) {
+      ret.push(
+        <PaginationItem>
+          <PaginationLink previous href={this.redirectUrl(this.state.category_id, page_id - 1, this.state.order, this.state.search)} />
+        </PaginationItem>
+      );
+    }
+
+    for(var id = Math.max(page_id - PAGE_NUM, 1);id <= Math.min(page_id + PAGE_NUM, TOTAL_PAGE);id++) {
+      if(page_id == id) {
+        ret.push(
+        <PaginationItem active>
+          <PaginationLink>
+            {id}
+          </PaginationLink>
+        </PaginationItem>
+        );
+      }
+      else {
+        ret.push(
+          <PaginationItem>
+            <PaginationLink href={this.redirectUrl(this.state.category_id, id, this.state.order, this.state.search)}>
+              {id}
+            </PaginationLink>
+          </PaginationItem>
+          );
+      }
+
+      if(id % 10 == 0) {
+        ret.push(<br />);
+      }
+    }
+
+    if(page_id != TOTAL_PAGE) {
+      ret.push(
+        <PaginationItem>
+          <PaginationLink next href={this.redirectUrl(this.state.category_id, page_id + 1, this.state.order, this.state.search)} />
+        </PaginationItem>
+      );
+    }
+    ret.push(
+      <PaginationItem>
+        <PaginationLink last href={this.redirectUrl(this.state.category_id, TOTAL_PAGE, this.state.order, this.state.search)} />
+      </PaginationItem>
+    );
+
+    return ret;
+  }
+
+  addCategory() {
+    var ret = [];
+    
+    ret.push(
+      <option value={0}>
+        전체
+      </option>
+    );
+
+    for(var i=0;i<this.state.categories.length;i++) {
+      ret.push(
+        <option key={this.state.categories[i].category_id} value={this.state.categories[i].category_id}>
+          {this.state.categories[i].name}
+        </option>
+      );
+    }
+
+    return ret;
+  }
 
   render() {
     return (
       <>
         <SimpleNavbar />
         <main ref="main">
-          <section className="section section-lg section-shaped pb-250">
-            {/* Circles background */}
-            <div className="shape shape-style-1 shape-default alpha-4">
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-            </div>
-            <Container className="py-lg-md d-flex">
-              <div className="col px-0">
-                <Row>
-                  <Col className="text-center" lg="6">
-                    <h1 className="display-3 text-white">&nbsp;</h1>
-                  </Col>
-                </Row>
-              </div>
-            </Container>
-
-            {/* SVG separator */}
-            <div className="separator separator-bottom separator-skew">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>
-            </div>
-          </section>
+          <Template />
           <Container>
             <Card className="card-profile shadow mt--300">
               <div className="px-4">
                 <Form>
                   <br />
-                  <h1 className="display-3 text-center">문제 목록</h1>
-                  <br />
+                  <h1 className="display-3 text-center">{this.getCategoryName(parseInt(this.state.category_id))} 게시글 목록</h1>
                   <Container>
-
-                    <ListGroup flush>
-                      <ListGroupItem disabled tag="a" href="#"><Row>
-                        <Col xs="auto">
-                          <ButtonGroup size="lg">
-                            <Button>인기순</Button>
-                            <Button>조회수순</Button>
-                            <Button>날짜순</Button>
-                          </ButtonGroup>
-                        </Col>
-                      </Row></ListGroupItem>
-                      <ListGroupItem>
-                        <Row>
-                          <Col xs="9">
-                            <a href={"problem-view-page/"}>배치가 가운데로 잘 안되는듯</a>
-                            <Row>
-                              <Col xs="8">
-                                <ButtonGroup size="sm">
-                                  <Button href = '#'>자료구조</Button>
-                                </ButtonGroup>
-                              </Col>
-                              <Col xs="4">게시일 : 2020.01.02</Col>
-                            </Row>
-                          </Col>
-                          <Col xs="1">111<Badge color="light">추천</Badge></Col>
-                          <Col xs="1">111<Badge color="light">답변</Badge></Col>
-                        </Row>
-                      </ListGroupItem>
-                      <ListGroupItem>
-                        <Row>
-                          <Col xs="9">
-                            <a href={"problem-view-page/"}>배치가 가운데로 잘 안되는듯</a>
-                            <Row>
-                              <Col xs="8">
-                                <ButtonGroup size="sm">
-                                  <Button href = '#'>OS</Button>
-                                </ButtonGroup>
-                              </Col>
-                              <Col xs="4">게시일 : 2020.01.03</Col>
-                            </Row>
-                          </Col>
-                          <Col xs="1">222<Badge color="light">추천</Badge></Col>
-                          <Col xs="1">222<Badge color="light">답변</Badge></Col>
-                        </Row>
-                      </ListGroupItem>
-                    </ListGroup>
-
-
-
-
-                    {
-                      this.state.isLogin &&
-                      <div className="text-center">
-                        <Button
-                          className="mt-4"
-                          color="primary"
-                          type="button"
-                          href="/problem-edit-page"
-                        >
-                          문제 추가버튼 : 링크를 위해 남겨놓음
-                        </Button>
-                      </div>
-                    }
+                    <Row>
+                      <Col xs="4">
+                      <Input type="select" value={this.state.category_id} onChange={this.handleCategoryId}>
+                        {this.addCategory()}
+                      </Input>
+                      </Col>
+                      <Col>
+                        <div className="text-right">
+                          <Button disabled={false}>
+                            추천순
+                          </Button>
+                          <Button disabled={false}>
+                            최신순
+                          </Button>
+                        </div>
+                      </Col>
+                    </Row>
                     <br />
-
+                    <ListGroup>
+                      {this.getList()}
+                    </ListGroup>
+                    <Row>
+                      <Col>
+                        <br />
+                        <Row>
+                          <Col xs="8">
+                            <FormGroup>
+                              <InputGroup className="mb-4">
+                                <InputGroupAddon addonType="prepend">
+                                  <InputGroupText>
+                                    <i className="ni ni-zoom-split-in" />
+                                  </InputGroupText>
+                                </InputGroupAddon>
+                                <Input placeholder="Search" type="text" value={this.state.search_text} onChange={this.handleSearchText}/>
+                              </InputGroup>
+                            </FormGroup>
+                          </Col>
+                          <Col xs="4">
+                            <Button color="default" type="button"
+                              href={this.redirectUrl(this.state.category_id, this.state.page, this.state.order, this.state.search_text)}>
+                              검색
+                            </Button>
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col>
+                      {
+                        (this.state.is_admin || this.state.is_prime) &&
+                        <div className="text-right">
+                          <Button
+                            className="mt-4"
+                            color="primary"
+                            type="button"
+                            href="/problem-edit-page"
+                          >
+                            게시글 추가
+                          </Button>
+                        </div>
+                      }
+                      </Col>
+                    </Row>
+                    <Row xs="3">
+                      <Col />
+                      <Pagination>
+                        {this.getPagination()}
+                      </Pagination>
+                      <Col />
+                    </Row>
                   </Container>
-                  <Row xs="3">
-                    <Col></Col>
-                    <Col>                      
-                    <Pagination aria-label="Page navigation example">
-                      <PaginationItem disabled>
-                        <PaginationLink first href="#" />
-                      </PaginationItem>
-                      <PaginationItem disabled>
-                        <PaginationLink previous href="#" />
-                      </PaginationItem>
-                      <PaginationItem active>
-                        <PaginationLink href="#">
-                          1
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink href="#">
-                          2
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink href="#">
-                          3
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink href="#">
-                          4
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink href="#">
-                          5
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink next href="#" />
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink last href="#" />
-                      </PaginationItem>
-                    </Pagination>
-                    </Col>
-                    <Col></Col>
-                  </Row>
                 </Form>
               </div>
             </Card>
